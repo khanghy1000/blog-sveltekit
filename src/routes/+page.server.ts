037c5f5d4b2ced8645c9a10cd03cd42c191176f2
khanghy1000 <khanghy130603@gmail.com>
2023-11-01 09:19:20 +0700

    feat: add pagination for post list

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,14 +1,23 @@
 import { prisma } from '$lib/server/prisma';
 import type { PageServerLoad } from './$types';
 
-export const load = (async ({ setHeaders }) => {
+export const load = (async ({ setHeaders, url }) => {
   setHeaders({
     'Cache-Control': 'max-age=0, s-maxage=60'
   });
+
+  const page = url.searchParams.get('page');
+  const parsedPage = page && +page > 0 ? +page - 1 : 0;
+
   const posts = await prisma.post.findMany({
     orderBy: {
       createdAt: 'desc'
-    }
+    },
+    take: 10,
+    skip: parsedPage * 10
   });
-  return { posts };
+
+  const postCount = await prisma.post.count();
+
+  return { posts, postCount };
 }) satisfies PageServerLoad;
